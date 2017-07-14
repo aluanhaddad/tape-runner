a7984e6d999b7bdcf3bc7ee38b6501bef0af8c3f
Aluan Haddad <aluanh@gmail.com>
2017-07-14 13:54:04 -0400

    response to explicit command

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,10 @@
 'use strict';
+import 'flat-map-polyfill';
+import 'core-js';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {TextDocument, Range, CodeActionContext, CancellationToken, ProviderResult, Command} from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,18 +13,49 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "tape-runner" is now active!');
+    context.subscriptions.push(vscode.languages.registerCodeActionsProvider(
+        {
+            language: 'typescript',
+            scheme: 'file',
+            pattern: `*.{ts,js}`
+        },
+        {
+            provideCodeActions: async (v: TextDocument, range: Range, context: CodeActionContext, token: CancellationToken) => {
 
+                const providerResults: Command[] = [
+                    {
+                        title: 'Run Test',
+                        command: 'extension.runTapeTests'
+
+                    }
+                ];
+                return providerResults;
+            }
+        }));
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    const command =
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+        vscode.commands.registerTextEditorCommand('extension.runTapeTests', async (textEditor, edit, ...args) => {
+            console.info(textEditor, edit);
+            console.info(args);
+            const {selection, document: {fileName}, options: {lineNumbers}, viewColumn} = textEditor;
+            const segmentToMatch = function () {
+                const segements = fileName && fileName.split(/\/|\\/).flatMap(s => [s.split('.')[0]]);
+                return segements[segements.length - 1];
+            }().split('.');
+            const testFiles = await vscode.workspace.findFiles(`**/${segmentToMatch}.spec.{ts,js}`, undefined, Infinity, undefined);
+            console.info(testFiles);
+            testFiles.forEach(file => {
+                vscode.commands.executeCommand('run test');
+            })
+        });
 
-    context.subscriptions.push(disposable);
+
+
+
+    context.subscriptions.push(command);
 }
 
 // this method is called when your extension is deactivated
